a568f1d77fe50b56c622c5a96fae36e23fa41310
Dio Woo <zqjimlove@gmail.com>
2021-04-11 17:45:16 +0800

    chore:debug

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const fs = require('fs')
 const { unique, downloadImg, homedir } = require('@imgcook/cli-utils')
 const path = require('path')
 const oss = require('ali-oss')
+const log4js = require('log4js')
+
+log4js.configure({
+  appenders: {
+    oss: {
+      type: 'file',
+      filename: path.join(homedir(), '.imgcook', 'oss.log'),
+    },
+  },
+  categories: { default: { appenders: ['oss'], level: 'debug' } },
+})
+
+const logger = log4js.getLogger('oss')
 
 let ossClient
 
@@ -17,13 +30,15 @@ const createOssClient = function (option) {
   !ossClient && (ossClient = new oss(option))
 }
 
-const uploadData = async (file, filepath, option) => {
+const uploadData = async (file, filepath) => {
   let result = await ossClient.put(`/static/` + filepath, file)
   return result
 }
 
 const pluginHandler = async (option) => {
+  logger.debug('config:', option.config)
   if (!option.config.oss) {
+    logger.err(`option.oss is not defined`, option)
     throw new Error('option.oss is not defined')
   }
   createOssClient(option.config.oss)
@@ -81,7 +96,10 @@ const pluginHandler = async (option) => {
           } else if (moduleData && moduleData.dsl === 'react-taobao-standard') {
             // If the local path image is referenced under the react standard, use the require reference
             const regex = new RegExp(`"${imgArr[idx]}"`, 'g')
-            fileValue = fileValue.replace(regex, `require('./images/${imgName}')`)
+            fileValue = fileValue.replace(
+              regex,
+              `require('./images/${imgName}')`
+            )
           } else {
             fileValue = fileValue.replace(reg, `./images/${imgName}`)
           }
@@ -116,5 +134,6 @@ const pluginHandler = async (option) => {
 module.exports = (...args) => {
   return pluginHandler(...args).catch((err) => {
     console.error(err)
+    logger.error(err)
   })
 }
